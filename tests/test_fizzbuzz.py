81ede535be96b7e11ede100c248c964bc0af46c4
Aram Simonian <aram.contacts@gmail.com>
2018-01-15 22:29:34 +0000

    Renamed first test to be more specific

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -2,7 +2,7 @@ import unittest
 from fizzbuzz import Fizzbuzz
 
 class FizzbuzzTest(unittest.TestCase):
-  def test_returns_fizz(self):
+  def test_returns_fizz_with_value_3(self):
     game = Fizzbuzz()
     result = game.play(3)
 
