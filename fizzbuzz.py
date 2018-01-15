525c91ef3b765f8b98a0ffb9232e0962c7d54461
Aram Simonian <aram.contacts@gmail.com>
2018-01-15 22:35:14 +0000

    Added and passed test for 5 & buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,5 @@ class Fizzbuzz(object):
   def play(self, number):
     if number%3 == 0:
       return 'fizz'
+    elif number%5 == 0:
+      return 'buzz'
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -11,3 +11,8 @@ class FizzbuzzTest(unittest.TestCase):
     game = Fizzbuzz()
     result = game.play(9)
     self.assertEqual('fizz', result)
+
+  def test_returns_fizz_with_value_5(self):
+    game = Fizzbuzz()
+    result = game.play(5)
+    self.assertEqual('buzz', result)
